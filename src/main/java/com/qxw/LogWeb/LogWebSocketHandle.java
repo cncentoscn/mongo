7a5f5518f61dffd01a8a03ad539f7e24068f294c
qinxuewu <870439570@qq.com>
2018-07-10 23:44:15 +0800

    WebSocket + tail命令实现Web实时日志

---
+++ src/main/java/com/qxw/LogWeb/LogWebSocketHandle.java
@@ -0,0 +1,66 @@
+package com.qxw.LogWeb;
+
+import java.io.IOException;
+import java.io.InputStream;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * WebSocket + tail命令实现Web实时日志
+ *
+ * JSR 356是Java实现WebSocket的一套规范，所以需要一个支持JSR 356的服务器，例如Tomcat、Jetty的最新版本。
+ *
+ * @author qinxuewu
+ * @create 18/7/10下午11:09
+ * @since 1.0.0
+ */
+
+
+@ServerEndpoint("/log")
+public class LogWebSocketHandle {
+
+    private Process process;
+    private InputStream inputStream;
+
+    /**
+     * 新的WebSocket请求开启
+     */
+    @OnOpen
+    public void onOpen(Session session) {
+        try {
+            System.out.println("----------webSocket--------------");
+            // 执行tail -f命令
+            process = Runtime.getRuntime().exec("tail -f /var/log/syslog");
+            inputStream = process.getInputStream();
+
+            // 一定要启动新的线程，防止InputStream阻塞处理WebSocket的线程
+            TailLogThread thread = new TailLogThread(inputStream, session);
+            thread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * WebSocket请求关闭
+     */
+    @OnClose
+    public void onClose() {
+        try {
+            if(inputStream != null)
+                inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if(process != null)
+            process.destroy();
+    }
+
+    @OnError
+    public void onError(Throwable thr) {
+        thr.printStackTrace();
+    }
+}
